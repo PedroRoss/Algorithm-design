610c1dd1395290828e04a06c49a103d57000e14c
PedroRoss <plrosasT@hotmail.com>
2024-04-07 03:23:14 -0600

    Carga código fuente al programa libre y la info del readme

---
+++ programa_libre.c
@@ -0,0 +1,10 @@
+/*Algoritmo para mostrar en pantalla la longitud de una
+circunferencia de radio 5*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("La longitud de una circunferencia de radio 5 es: %f", 2 * 3.1416 * 5);
+    return 0;
+}
