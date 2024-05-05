6671cdf8fb13623dd88cc97e76f061915808a32f
PedroRoss <165524170+PedroRoss@users.noreply.github.com>
2024-05-05 11:39:49 +0000

    implementacion de datos no aceptados

--- zodiacal.c
+++ zodiacal.c
@@ -12,8 +12,15 @@ int main()
     printf("Ingresa el mes de tu nacimiento (1-12): ");
     scanf("%d", &mes);
 
-    printf("Tu signo zodiacal es: ");
-    signo_zodiacal(dia, mes);
+    if (dia <= 31 && mes <= 12)
+    {
+        printf("Tu signo zodiacal es: ");
+        signo_zodiacal(dia, mes);
+    }
+    else
+    {
+        printf("No se ha ingresado una fecha correcta \n");
+    }
 
     return 0;
 }
@@ -21,27 +28,27 @@ int main()
 void signo_zodiacal(int dia, int mes)
 {
     if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18))
-        printf("Acuario");
+        printf("Acuario \n");
     else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20))
-        printf("Piscis");
+        printf("Piscis \n");
     else if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19))
-        printf("Aries");
+        printf("Aries \n");
     else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20))
-        printf("Tauro");
+        printf("Tauro \n");
     else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20))
-        printf("Géminis");
+        printf("Géminis \n");
     else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22))
-        printf("Cáncer");
+        printf("Cáncer \n");
     else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22))
-        printf("Leo");
+        printf("Leo \n");
     else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22))
-        printf("Virgo");
+        printf("Virgo \n");
     else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22))
-        printf("Libra");
+        printf("Libra \n");
     else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21))
-        printf("Escorpio");
+        printf("Escorpio \n");
     else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21))
-        printf("Sagitario");
+        printf("Sagitario \n");
     else
-        printf("Capricornio");
+        printf("Capricornio \n");
 }
